9affcf192bfdf775808ea25115b5849a534d5519
WSaffery <William.Saffery@gmail.com>
2022-01-22 19:07:06 +1100

    shadowss

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -7,22 +7,22 @@ function classString(mainCls, subClasses) {
   return `${mainCls} ${mainCls}-${subClasses.join(` ${mainCls}-`)}`;
 }
 
-function ShowButton(props) {
-  return (<><button className={props.className}>Show {props.status}</button></>);
-}
 
 function Experience(props) {
   const subClasses = Object.keys(props)
                     .filter( key => props[key] == "subclass");
-  console.log(props);
-  console.log(subClasses);
+  // console.log(props);
+  // console.log(subClasses);
+  if (props.shade) {
+    subClasses.push("shade");
+  }
   const defaultCls = classString("Experience", subClasses);
   const bigCls = classString("Experience_hovered", subClasses);
   const [cls, setCls] = React.useState(defaultCls);
   const [mouse, setMouse] = React.useState(0);
 
   function MouseDown() {
-    console.log(props.index)
+    // console.log(props.index)
     props.callup(props.index, 1)
   }
 
@@ -39,27 +39,23 @@ function Experience(props) {
     // backToSmall();
   }
 
-  if (props.big == 1) {
+  if (props.big > 0) {
     props.setOnBig(mouse);
   }
 
-  if (props.big == 1 && cls == defaultCls) {
+  if (props.big > 0 && cls == defaultCls) {
     props.setOnBig(1);
     setCls(bigCls);
   }
 
-  // else if (props.big == 1 && mouse != 1) {
-  //   backToSmall();
-  // }
-  // else if (props.big == 0 && mouse != 1 && cls != defaultCls) {
-  //   setCls(defaultCls);
-  // }
   if (props.big == -1 && cls != defaultCls) {
     setCls(defaultCls);
   }
 
-  const wrapTextCls = cls == bigCls?"experienceTextDiv_Big":"experienceTextDiv";
-
+  var wrapTextCls = cls == bigCls?"experienceTextDiv_Big":"experienceTextDiv";
+  if (props.shade) {
+    wrapTextCls += " ShadowCover"
+  }
   // const wrapper = `${defaultCls}_wrapper`;
 
   return (
@@ -77,70 +73,80 @@ function Experience(props) {
     {props.comment.map( (x) => <><br/> {x}</>)}
     </div>
     </div>
-    <ShowButton status="more" className="Show"/>
     </div>
   </li>
   );
 }
 
+// <ShadowCover/>
+// <ShowButton status="more" className="Show"/>
+
+function Monitor(props) {
+  const subClasses = Object.keys(props)
+                    .filter( key => props[key] == "subclass");
+  const defaultCls = classString("Experience", subClasses);
+  const cls = classString("Experience_hovered", subClasses);
+  const wrapTextCls = "experienceTextDiv_Big"
+  // const [cls, setCls] = React.useState(defaultCls);
+  // const wrapTextCls = cls == bigCls?"experienceTextDiv_Big":"experienceTextDiv";
+
+  if (props.live != -1) {
+    return (
+      <li className="cards__item">
+      <div className={cls}>
+       <div className={wrapTextCls}>
+       {props.name}<br/>
+       <span className="time">{props.time}</span>
+       <CenteredImage className="ExperienceImage" src={props.image} alt={props.name}/>
+       {props.description}
+       <div className="comment">
+       {props.comment.map( (x) => <><br/> {x}</>)}
+       </div>
+       </div>
+       </div>
+     </li>
+    );
+  }
+  else {
+    return null;
+  }
+
+}
 
 function Experiences() {
   const [big, setBig] = React.useState(0);
   const [data, setData] = React.useState(createData(ExperienceData));
   const [onBig, setOnBig] = React.useState(0);
+  const [live, setLive] = React.useState(-1);
 
   function createData(experienceData) {
-    return {"first":experienceData[0], "rest":experienceData.slice(1)};
+    return {"focus_index":0, "array":experienceData};
   }
 
   function shiftUp(index) {
     const newFirst = data.rest.splice(index,1)[0];
-    data.rest.unshift(data.first)
-    var newData = {"first":newFirst, "rest":data.rest}
+    data.rest.unshift(data.first);
+    var newData = {"first":newFirst, "rest":data.rest};
     setData(newData);
-  }
-  document.onclick = () => {
-    setTimeout(() => {if (onBig == 0 && big == 1) {
-          setBig(-1);
-        }}, 100)
-
-    // if (mouse === 0 && cls === bigCls) {
-    //   props.callup(props.index, 0)
-    // }
-    // else if (cls != bigCls) {
-    //   props.callup(props.index, 1)
-    // }
   }
 
   function callup(index, val) {
     if (val == true) {
-      if (index != -1) {
-        shiftUp(index);
-      }
-      setBig(1);
-    }
-    else {
-      setBig(0);
-      // setTimeout(() => {
-      //     setBig(0);
-      //   }, 1000);
-      // setBig(-1);
+      data.focus_index = index;
+      setLive(index);
     }
     return true;
   }
 
-  function waitAndCheck() {
-
-  }
-
-  // console.log(data);
-
   return (
+    <>
+    <Monitor live={live} key={data.array[data.focus_index]} big={1} setOnBig={setOnBig} index={-1} callup={(index, val) => callup(index, val)} {...data.array[data.focus_index]}/>
     <ul className="Experiences">
-    <Experience key={data.first} big={big} setOnBig={setOnBig} index={-1} callup={(index, val) => callup(index, val)} {...data.first}/>
-    {data.rest.map( (x) => <Experience key={x} big={-1} index={data.rest.indexOf(x)} callup={(index, val) => callup(index, val)} {...x}/>)}
+    {data.array.map( (x) => <Experience key={x} shade={(data.array.indexOf(x)==data.focus_index) && live != -1} big={-1} index={data.array.indexOf(x)} callup={(index, val) => callup(index, val)} {...x}/>)}
     </ul>
+    </>
   );
 }
+// <Experience key={data.array[data.focus_index]} big={1} setOnBig={setOnBig} index={-1} callup={(index, val) => callup(index, val)} {...data.array[data.focus_index]}/>
 
 export default Experiences;
